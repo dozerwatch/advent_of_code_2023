776e56eba656b985aa3d31f2c2d96b10568820a6
Hong Tan <dozerwatch@gmail.com>
2023-12-11 22:06:02 -0800

    Solve part 2

--- day7/part2/solution.py
+++ day7/part2/solution.py
@@ -1,9 +1,12 @@
-SUM, plays = 0, []
-lines = open('day7/test.txt').read().splitlines()
+SUM, plays= 0, []
+lines = open('day7/part2/data.txt').read().splitlines()
 for line in lines:
     hand, bid = line.split()
-    hand = hand.translate(str.maketrans('TJQKA', 'ABCDE'))
-    type = sorted([hand.count(card) for card in hand], reverse=True)
+    hand = hand.translate(str.maketrans('TJQKA', 'A0CDE'))
+    replacement_hands = [hand.replace('0', r) for r in hand]
+    counts = [[h.count(x) for h in replacement_hands for x in h][x:x+5] for x in range(0,25,5)]
+    sorted_hands = [sorted(h, reverse=True) for h in counts]
+    type = max(sorted_hands)
     plays.append((type, hand, int(bid)))
 for rank, (type, hand, bid) in enumerate(sorted(plays), 1):
     SUM += rank * bid
